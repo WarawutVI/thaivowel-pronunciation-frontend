c426e5cdab0809abafb472487550b93ccb11b8ae
Warawut Vichaiya <warawutvichaiya@gmail.com>
2026-05-28 23:01:11 +0700

    Revert "deploy"
    This reverts commit 5bfcc8a179f6ca27627fc977d273d3a0609c2a36.

--- lib/services/practice_api.dart
+++ lib/services/practice_api.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-const String _base = 'https://perkiness-shadiness-extras.ngrok-free.dev';
-const String _flaskBase = 'https://perkiness-shadiness-extras.ngrok-free.dev';
+const String _base = 'http://10.0.2.2:4000';
+const String _flaskBase = 'http://192.168.0.11:5000';
 
 class VowelDetail {
   final int id;
